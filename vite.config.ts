4c8749f442e5e88f368c4070abc633899493c40d
Tim Suvorov <timiniev@outlook.com>
2025-01-28 20:47:06 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/myPortfolio/',
   build: {
     outDir: 'dist', // Change this if needed
   },
